916815472f1b1c212dd0facc4f1d693536c84008
azeli <avzeli@mail.ru>
2016-11-22 20:22:00 +0300

    Add files via upload

---
+++ project3/TableViewController.m
@@ -0,0 +1,75 @@
+//
+//  UITableViewController+TableViewController.m
+//  project3
+//
+//  Created by Анна  Зелинская on 08.11.16.
+//  Copyright © 2016 Анна  Зелинская. All rights reserved.
+//
+
+#import "TableViewController.h"
+#import "ViewController.h"
+
+@interface TableViewController ()
+
+@end
+
+@implementation TableViewController
+
+@synthesize arrayData1;
+
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+#pragma mark - Table view data source
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    
+    return 1;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    
+    return [arrayData1 count];
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    static NSString *CellIdentifier = @"CellIdentifier";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    cell.textLabel.attributedText = [arrayData1 objectAtIndex:indexPath.row];
+    
+    return cell;
+    
+}
+
+- (void) dealloc{
+    
+    [arrayData1 release];
+    
+    arrayData1 = nil;
+    
+    [super dealloc];
+    
+}
+
+
+@end
